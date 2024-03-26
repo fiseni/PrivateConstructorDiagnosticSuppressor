d46abf1bba017f0b069e4fa81a8722d58442d787
Fati Iseni <fati.iseni@pozitrongroup.com>
2024-03-26 07:35:04 +0100

    Added diagnostic suppressor implementation.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pozitron.Analyzers.PrivateConstructorDiagnosticSuppression
---
+++ src/PrivateConstructorDiagnosticSuppressor/PrivateConstructorNullabilitySuppressor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Pozitron.Roslyn.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class PrivateConstructorNullabilitySuppressor : DiagnosticSuppressor
+{
+    public static SuppressionDescriptor SuppressionDescriptor => new(
+        id: "POZ0001",
+        suppressedDiagnosticId: "CS8618",
+        justification: "The state is set externally by 3rd party libraries (EF Core, mappers, etc).");
+
+    public override ImmutableArray<SuppressionDescriptor> SupportedSuppressions { get; } = ImmutableArray.Create(SuppressionDescriptor);
+
+    public override void ReportSuppressions(SuppressionAnalysisContext context)
+    {
+        foreach (var diagnostic in context.ReportedDiagnostics)
+        {
+            if (SuppressionDescriptor.SuppressedDiagnosticId.Equals(diagnostic.Id))
+            {
+                SyntaxNode? node = diagnostic.Location.SourceTree?
+                    .GetRoot(context.CancellationToken)
+                    .FindNode(diagnostic.Location.SourceSpan);
+
+                if (node is ConstructorDeclarationSyntax ctorDeclarationSyntax
+                    && ctorDeclarationSyntax.Modifiers.Any(modifier => modifier.IsKind(SyntaxKind.PrivateKeyword)))
+                {
+                    context.ReportSuppression(Suppression.Create(SuppressionDescriptor, diagnostic));
+                }
+            }
+        }
+    }
+}
